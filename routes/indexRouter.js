10da39730ea17d3ec3eab644088ed9e8ec391827
Stephen <stephensouness@outlook.com>
2025-02-09 13:43:52 +0000

    messages array now renders in index / with index.ejs file

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,12 +7,12 @@ const messages = [
   { text: "Not bad Henry, not bad!", user: "Hans Capon ", added: new Date() },
   { text: "Could be better", user: "Katherine ", added: new Date() },
   { text: "Let's drink!", user: "Father Godwin ", added: new Date() },
+  { text: "Woof!", user: "Mutt ", added: new Date() },
   { text: "What you going to do??", user: "Markvart ", added: new Date() },
   { text: "You'll never get your sword back!", user: "Istvan Toth ", added: new Date() },
   { text: "I'll Avennge him", user: "Erik ", added: new Date() },
-  { text: "Woof!", user: "Mutt ", added: new Date() },
 ];
 
-indexRouter.get("/", (req, res) => res.send("Message Index"));
+indexRouter.get("/", (req, res) => res.render("index", { messages: messages }));
 
 module.exports = indexRouter;
