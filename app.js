c448cb75e0341effd77e061c8afa0419cee9bbea
Stephen <stephensouness@outlook.com>
2025-02-09 12:11:25 +0000

    ejs set up and main folders of component

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.send("Hi there, how's it going?"));
 
